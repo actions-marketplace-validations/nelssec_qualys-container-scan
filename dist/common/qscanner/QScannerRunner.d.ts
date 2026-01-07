ac51dfa7e2c819eacca8ad4cddaac80b586735c2
Andrew Nelson <nelson84.03@gmail.com>
2026-01-06 20:34:02 -0800

    Add compiled action

---
+++ dist/common/qscanner/QScannerRunner.d.ts
@@ -0,0 +1,28 @@
+import { QScannerConfig, QScannerResult, ContainerScanOptions, SarifReport, VulnerabilitySummary } from '../types';
+export declare class QScannerRunner {
+    private config;
+    private binaryPath;
+    private workDir;
+    private accessToken;
+    constructor(config: QScannerConfig);
+    setup(): Promise<void>;
+    private gunzipFile;
+    private calculateSha256;
+    private authenticate;
+    scanImage(options: ContainerScanOptions): Promise<QScannerResult>;
+    parseSarifReport(reportPath: string): {
+        summary: VulnerabilitySummary;
+        report: SarifReport;
+    };
+    getBinaryPath(): string | null;
+    getWorkDir(): string;
+    cleanup(): void;
+    private buildCommonArgs;
+    private executeQScanner;
+    private buildResult;
+    private getPlatform;
+    private getArchitecture;
+    private downloadFile;
+}
+export default QScannerRunner;
+//# sourceMappingURL=QScannerRunner.d.ts.map
